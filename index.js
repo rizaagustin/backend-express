7e3343f053a4b579a85722aaaff91ceac7ed95bf
Riza Agustin <rizaagustin20@gmail.com>
2024-06-21 10:08:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+//init app
+const app = express();
+
+//use cors
+app.use(cors());
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//define port
+const port = 3000;
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port,() => {
+  console.log(`Server started on port ${port}`);
+});
